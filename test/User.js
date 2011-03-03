f9f620eaf59059677135c1a25aeac6fefaca82ec
maritz <maritz.peters@gmail.com>
2011-03-03 18:22:37 +0100

    Fix tests to work again

--- test/User.js
+++ test/User.js
@@ -1,102 +1,150 @@
 require.paths.unshift(__dirname + '/../ni/lib');
-var Ni = require('ni');
+var Ni = require('ni')
+, nohm = require('nohm')
+, User = require(__dirname+'/../models/User')
+, nohmclient
+, assert = require('assert')
+, util = require('util')
+, timeout = 0;
 
+var args = process.argv.slice(2)
+, startInstructions = 'You need to start the tests with "expresso -s -t 8000" or "expresso -s --no-timeout-check" if you know what you\'re doing.';
+if (args.indexOf('-s') === -1 && args.indexOf('--serial') === -1) {
+  console.log(startInstructions);
+  process.exit(1);
+}
+if (args.indexOf('-t') === -1 && args.indexOf('--timeout') === -1 && args.indexOf('--no-timeout-check') === -1) {
+  console.log(startInstructions);
+  process.exit(1);
+} else if (args.indexOf('--no-timeout-check') === -1){
+  var arg = args.indexOf('-t') === -1 ? '--timeout' : '-t'
+  , pos = args.indexOf(arg);
+  timeout = parseInt(args[pos+1], 10);
+  console.log(timeout);
+  if (timeout < 8000) {
+    console.log(startInstructions);
+    process.exit(1);
+  }
+}
 
 // load config
-require('./config');
+require(__dirname+'/../config');
 
-var nohmclient = nohm.setPort(Ni.config('redis_port'));
-nohmclient.select(Ni.config('redis_nohm_db'), function (err) {
-  if (err) {
-    console.dir(err);
-    process.exit();
-  }
-});
+nohm.connect(Ni.config('redis_port'), Ni.config('redis_host'));
+nohmclient = nohm.getClient();
 
-exports = {
-  'salt properly written/read': function (t, done) {
+module.exports = {
+  'flushdb warning': function (done) {
+    console.log('ATTENTION! This will flush db 0 of your configured redis server in '+(timeout/1000).toFixed(1)+' seconds!\n');
+    var time = +new Date()+timeout
+    , interval = setInterval(function () {
+      var current = +new Date();
+      if (current >= time)
+        return clearInterval(interval);
+      util.print('\r'+((time-current)/1000).toFixed(1));
+    }, 100);
+    setTimeout(function () {
+      nohmclient.flushdb();
+      done();
+    }, timeout);
+  },
+  'salt properly written/read': function (done) {
     var user = new User(),
     control = new User(),
     expect = 0;
     user.p({
       name: 'salt',
-      email: 'salt@test.st',
       password: 'hurgelwurz'
     });
     user.save(function (err) {
-      t.equal(err, null);
+      if (err == 'invalid')
+        console.dir(user.errors);
+      assert.equal(err, null);
       control.load(user.id, function (err) {
-        t.equal(err, null);
-        t.equal(user.p('salt'), control.p('salt'));
+        assert.equal(err, null);
+        assert.equal(user.p('salt'), control.p('salt'));
         done(function () {
-          t.equal(expect, 0, 'not all async tests ran');
-        })
-      })
-    })
+          console.log('exiting');
+          assert.equal(expect, 0, 'not all async tests ran');
+        });
+      });
+    });
   },
-  'salt changes password': function (t, done) {
-    var user = new User(),
-    control = new User();
-    user.p('password', 'test');
-    control.p('password', 'test');
-    t.notEqual(user.p('password'), control.p('password'));
+  'empty password invalidates': function (done) {
+    var user = new User();
+    user.p('password', '');
+    assert.equal(user.valid('password'),false);
+    assert.deepEqual(user.errors.password, ['notEmpty']);
     done();
   },
-  'changing the password changes the salt': function (t, done) {
+  'short password invalidates': function (done) {
+    var user = new User();
+    user.p('password', 'test');
+    assert.equal(user.valid('password'),false);
+    assert.deepEqual(user.errors.password, ['minLength']);
+    done();
+  },
+  'salt changes password': function (done) {
+    var user = new User(),
+    control = new User();
+    user.p('password', 'testtest');
+    control.p('password', 'testtest');
+    assert.notEqual(user.p('password'), control.p('password'));
+    done();
+  },
+  'changing the password changes the salt': function (done) {
     var user = new User(),
     oldSalt = user.p('salt');
     user.p('password', 'testssadsd');
-    t.notEqual(user.p('salt'), oldSalt);
+    assert.notEqual(user.p('salt'), oldSalt);
     done();
   },
-  'test login with faulty inputs': function (t, done) {
+  'test login with faulty inputs': function (done) {
     var user = new User(),
     expect = 1;
     user.login('login_faulty', 'asdasd', function (logged) {
-      t.equal(logged, false);
+      assert.equal(logged, false);
       expect--;
     });
     user.p({
       name: 'login_faulty',
-      email: 'login_faulty@test.st',
       password: 'hurgelwurz'
     });
     user.save(function (err) {
       user.login('login_faulty', 'asdasd', function (logged) {
-        t.equal(logged, false);
+        assert.equal(logged, false);
         done(function () {
-          t.equal(expect, 0, 'not all async tests ran');
-        })
+          assert.equal(expect, 0, 'not all async tests ran');
+        });
       });
     });
   },
-  'test login with correct inputs': function (t, done) {
+  'test login with correct inputs': function (done) {
     var user = new User(),
     expect = 0;
     user.p({
       name: 'login_correct',
-      email: 'login_correct@test.st',
+      email: 'login_correct@tesassert.st',
       password: 'asdasd'
     });
     user.save(function (err) {
       var id = user.id;
       user.login('login_correct', 'asdasd', function (logged) {
-        t.equal(logged, true);
-        t.equal(user.id, id);
+        assert.equal(logged, true);
+        assert.equal(user.id, id);
         done(function () {
-          t.equal(expect, 0, 'not all async tests ran');
+          assert.equal(expect, 0, 'not all async tests ran');
         });
       });
     });
   },
-  'test getting basic userbox infos': function (t, done) {
+  'test getting basic userbox infos': function (done) {
     // TODO: complete this once teams/privileges are implemented (by adding seperate tests)
     var user = new User(),
     control = new User();
     user.p({
       name: 'userbox',
-      email: 'userbox@mail.de',
-      password: 'asd'
+      password: 'asdasd'
     });
     user.save(function (err) {
       var id = user.id;
@@ -105,14 +153,14 @@ exports = {
           id: id,
           name: user.p('name')
         };
-        t.notEqual(data, false);
-        t.deepEqual(should, data);
+        assert.notEqual(data, false);
+        assert.deepEqual(should, data);
         done();
-      })
+      });
     });
   },
-  'end db connection': function (t) {
-    //nohm.client.flushdb();
-    nohm.client.quit();
+  'end db connection': function (done) {
+    nohmclient.quit();
+    done();
   }
 };
